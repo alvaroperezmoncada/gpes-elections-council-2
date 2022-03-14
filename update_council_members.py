bc1f40e2b45ac9384a8fb3f79aeb58cbd95854f6
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2022-03-13 21:37:35 -0500

    Se incluye estilo a las paginas de confirmación de presentación y alegación.
    Se crea script para actualización masiva de mensajeros.

--- update_council_members.py
+++ update_council_members.py
@@ -9,13 +9,15 @@ import pandas as pd
 from council_member.models import CouncilMember
 
 df = pd.read_csv('Datos Consejo.csv', sep=',')
-print(df)
 for i in df.index:
     email = df['Correo electrónico'][i]
     member, created = CouncilMember.objects.get_or_create(email=email)
     if created:
         member.firstname = df['Nombre'][i]
         member.lastname = df['Apellidos'][i]
-        member.dni_number = df['DNI'][i]
-        member.save()
+
+    member.dni_number = df['DNI'][i]
+    member.save()
+
+
 
